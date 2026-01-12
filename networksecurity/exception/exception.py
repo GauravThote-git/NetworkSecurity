9267f9273adb08a1c1c507f0cf3b9b0c9a36b5ba
GauravThote-git <gthote24@gmail.com>
2026-01-12 22:54:05 +0530

    logger and exception handeling updated

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -3,22 +3,33 @@ from networksecurity.logging import logger
 
 
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details: sys):
-        self.error_message = error_message
+    def __init__(self, error_message: Exception, error_details: sys):
+        super().__init__(error_message)
+
         _, _, exc_tb = error_details.exc_info()
 
-        self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        if exc_tb is not None:
+            self.lineno = exc_tb.tb_lineno
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+        else:
+            self.lineno = "Unknown"
+            self.file_name = "Unknown"
+
+        self.error_message = error_message
 
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-            self.file_name, self.lineno, str(self.error_message))
+        return (
+            f"Error occurred in python script name "
+            f"[{self.file_name}] line number "
+            f"[{self.lineno}] error message "
+            f"[{self.error_message}]"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        logger.logging.info("Enter the try block")
-        a = 1/0
-        print("This will not be printed", a)
+        logger.info("Enter the try block")
+        a = 1 / 0
     except Exception as e:
+        logger.error("Exception occurred", exc_info=True)
         raise NetworkSecurityException(e, sys)
